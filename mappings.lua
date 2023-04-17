1f0487d6aa48fc27579c6c0a66c56048c389d365
Shoichi Ishida <sho1i4da@gmail.com>
2023-04-18 02:21:07 +0900

    update

--- mappings.lua
+++ mappings.lua
@@ -2,6 +2,12 @@ return {
   i = {
     -- コントロールキーを推しながらaを押すと、おすすめを受け入れる
     -- ["<C-a>"] = { 'copilot#Accept(<Tab>)', silent = true, expr = true, script = true },
+    -- option + a
     ["å"] = { 'copilot#Accept("<CR>")', noremap = true, silent = true, expr = true, script = true },
+    -- option + >
+    ["≥"] = { '<Plug>(copilot-next)', silent = true},
+    -- option + <
+    ["≤"] = { '<Plug>(copilot-previous)', silent = true} 
+
   }
 }
