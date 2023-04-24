8bd825ac8d421c11789a4f467f17dfb1533a9006
Shoichi Ishida <sho1i4da@gmail.com>
2023-04-24 12:28:30 +0900

    update

--- mappings.lua
+++ mappings.lua
@@ -1,13 +1,17 @@
 return {
   i = {
-    -- コントロールキーを推しながらaを押すと、おすすめを受け入れる
-    -- ["<C-a>"] = { 'copilot#Accept(<Tab>)', silent = true, expr = true, script = true },
-    -- option + a
-    ["å"] = { 'copilot#Accept("<CR>")', noremap = true, silent = true, expr = true, script = true },
-    -- option + >
+    ["<C-i>"] = { '<Plug>copilot#Accept("<CR>")', noremap = true, silent = true, expr = true, script = true },
     ["≥"] = { '<Plug>(copilot-next)', silent = true},
-    -- option + <
-    ["≤"] = { '<Plug>(copilot-previous)', silent = true} 
-
+    ["≤"] = { '<Plug>(copilot-previous)', silent = true},
+  },
+  n = {
+   ["<S-l>"] = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    ["<S-h>"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    }, 
   }
 }
