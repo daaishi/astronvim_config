0d60660404fe0bf1a76c4961473358f9f63f1841
Shoichi Ishida <sho1i4da@gmail.com>
2023-05-12 14:59:15 +0900

    add community

--- init.lua
+++ init.lua
@@ -4,6 +4,12 @@ return {
     formatting = {
       format_on_save = {
         enabled = true, -- enable format on save
+        allow_filetypes = { -- enable format on save for specified filetypes only
+          -- "go",
+        },
+        ignore_filetypes = { -- disable format on save for specified filetypes
+          "typescript",
+        },
       },
       filter = function(client)
         if client.name == "lua_ls" then
@@ -13,52 +19,52 @@ return {
         if vim.bo.filetype == "javascript" then
           return client.name == "null-ls"
         end
-
+        
         return true
       end
     },
     servers = {
-      "dartls",
+      -- "dartls",
     },
     setup_handlers = {
       -- add custom handler
-      dartls = function(_, opts) require("flutter-tools").setup { lsp = opts } end,
-      tsserver = function(_, opts) require("typescript").setup { server = opts } end
+      -- dartls = function(_, opts) require("flutter-tools").setup { lsp = opts } end,
+      -- tsserver = function(_, opts) require("typescript").setup { server = opts } end
     },
     config = {
-      dartls = {
-        -- any changes you want to make to the LSP setup, for example
-        color = {
-          enabled = true,
-        },
-        settings = {
-          showTodos = true,
-          completeFunctionCalls = true,
-        },
-      },
+      -- dartls = {
+      --   -- any changes you want to make to the LSP setup, for example
+      --   color = {
+      --     enabled = true,
+      --   },
+      --   settings = {
+      --     showTodos = true,
+      --     completeFunctionCalls = true,
+      --   },
+      -- },
     },
   },
-  plugins = {
-    "akinsho/flutter-tools.nvim", -- add lsp plugin
-    "jose-elias-alvarez/typescript.nvim", -- add lsp plugin
-    {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-        ensure_installed = { "tsserver" }, -- automatically install lsp
-      },
-    },
-    {
-      "github/copilot.vim",
-      cmd = "Copilot",
-      event = "InsertEnter",
-      config = function() vim.g.copilot_no_tab_map = true end,
-    },
-    {
-      "catppuccin/nvim",
-      as = "catppuccin",
-      config = function()
-        require("catppuccin").setup {}
-      end,
-    },
-  },
+  -- plugins = {
+  --   -- "akinsho/flutter-tools.nvim", -- add lsp plugin
+  --   -- "jose-elias-alvarez/typescript.nvim", -- add lsp plugin
+  --   -- {
+  --   --   "williamboman/mason-lspconfig.nvim",
+  --   --   opts = {
+  --   --     ensure_installed = { "tsserver" }, -- automatically install lsp
+  --   --   },
+  --   -- },
+  --   -- {
+  --   --   "github/copilot.vim",
+  --   --   cmd = "Copilot",
+  --   --   event = "InsertEnter",
+  --   --   config = function() vim.g.copilot_no_tab_map = true end,
+  --   -- },
+  --   -- {
+  --   --   "catppuccin/nvim",
+  --   --   as = "catppuccin",
+  --   --   config = function()
+  --   --     require("catppuccin").setup {}
+  --   --   end,
+  --   -- },
+  -- },
 }
--- mappings.lua
+++ mappings.lua
@@ -1,9 +1,9 @@
 return {
-  i = {
-    ["<C-l>"] = { 'copilot#Accept("<CR>")', noremap = true, silent = true, expr = true, script = true, replace_keycodes = false},
-    ["≥"] = { '<Plug>(copilot-next)', silent = true},
-    ["≤"] = { '<Plug>(copilot-previous)', silent = true},
-  },
+  -- i = {
+  --   ["<C-l>"] = { 'copilot#Accept("<CR>")', noremap = true, silent = true, expr = true, script = true, replace_keycodes = false},
+  --   ["≥"] = { '<Plug>(copilot-next)', silent = true},
+  --   ["≤"] = { '<Plug>(copilot-previous)', silent = true},
+  -- },
   n = {
    ["<S-l>"] = {
       function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
