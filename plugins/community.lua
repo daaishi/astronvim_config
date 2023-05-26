13d75c234aceb892852839315cad739372ce2d20
Shoichi Ishida <sho1i4da@gmail.com>
2023-05-26 10:43:32 +0900

    背景を透明にする

--- plugins/community.lua
+++ plugins/community.lua
@@ -8,23 +8,24 @@ return {
   { import = "astrocommunity.colorscheme.kanagawa", enabled = true },
   { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { -- further customize the options set by the community
-    "catppuccin",
-    opts = {
-      integrations = {
-        sandwich = false,
-        noice = true,
-        mini = true,
-        leap = true,
-        markdown = true,
-        neotest = true,
-        cmp = true,
-        overseer = true,
-        lsp_trouble = true,
-        ts_rainbow2 = true,
-      },
-    },
-  },
+  { import = "astrocommunity.colorscheme.tokyonight" },
+  -- { -- further customize the options set by the community
+  --   "catppuccin",
+  --   opts = {
+  --     integrations = {
+  --       sandwich = false,
+  --       noice = true,
+  --       mini = true,
+  --       leap = true,
+  --       markdown = true,
+  --       neotest = true,
+  --       cmp = true,
+  --       overseer = true,
+  --       lsp_trouble = true,
+  --       ts_rainbow2 = true,
+  --     },
+  --   },
+  -- },
   { import = "astrocommunity.completion.copilot-lua" },
   { -- further customize the options set by the community
     "copilot.lua",
